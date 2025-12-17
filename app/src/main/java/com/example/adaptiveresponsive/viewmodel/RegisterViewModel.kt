c2a5cf1a775c7dbf6460e5cbe3f7281c14038a33
PauSGracia <pau.sole@gracia.lasalle.cat>
2025-12-17 17:18:43 +0100

    feat: add signup to viewmodel

--- app/src/main/java/com/example/adaptiveresponsive/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/example/adaptiveresponsive/viewmodel/RegisterViewModel.kt
@@ -1,7 +1,11 @@
 package com.example.adaptiveresponsive.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.adaptiveresponsive.model.SignUp
 
 class RegisterViewModel : ViewModel() {
-
+    private val _signUp = MutableLiveData<SignUp>()
+    val signUp: LiveData<SignUp> = _signUp
 }

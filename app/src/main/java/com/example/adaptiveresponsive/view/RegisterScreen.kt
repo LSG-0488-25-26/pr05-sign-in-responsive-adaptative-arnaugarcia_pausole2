dac97f2c8073e5aecc367741b0afdbb730680105
PauSGracia <pau.sole@gracia.lasalle.cat>
2025-12-17 16:18:53 +0100

    feat: create register screen view

---
+++ app/src/main/java/com/example/adaptiveresponsive/view/RegisterScreen.kt
@@ -0,0 +1,10 @@
+package com.example.adaptiveresponsive.view
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.adaptiveresponsive.viewmodel.RegisterViewModel
+
+@Composable
+fun RegisterScreen(navController: NavController, viewModel: RegisterViewModel) {
+}
+
